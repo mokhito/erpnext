7d913b53d876f6abf459b53d0020f0dc1f322091
mokhito <mokhito@users.noreply.github.com>
2019-04-24 13:58:50 +0200

    Change the way this report is computed.

--- erpnext/accounts/report/journal_des_achats/journal_des_achats.py
+++ erpnext/accounts/report/journal_des_achats/journal_des_achats.py
@@ -99,14 +99,14 @@ def execute(filters=None):
     },
     {
         "fieldname": "vat_refundable",
-        "label": _("TVA deductible"),
+        "label": _("TVA déductible"),
         "fieldtype": "Float",
         "options": "",
         "width": 120
     },
     {
         "fieldname": "grid_63",
-        "label": _("TVA à reverser"),
+        "label": _("TVA non déductible"),
         "fieldtype": "Float",
         "options": "",
         "width": 120
@@ -133,52 +133,40 @@ def get_invoices(filters):
     res_invoices = frappe.db.sql(query)
 
     for res in res_invoices:
-        bin_goods, bin_services, bin_investments, bin_notax, bin_cn, bin_intracom, bin_cn_intracom, is_intracom, is_import, bin_imports, bin_refundable, bin_payable = get_bins(res[1])
+        bins = get_bins(res[1])
 
         # if there is a mix of expenses, follow the belgian administration rule
         # of 50% (see book "Apprendre la T.V.A.")
-        if bin_goods > 0.0 and bin_services > 0.0:
-            if bin_goods < (0.5 * res[4]):
-                bin_services += bin_goods
-                bin_goods = 0.0
+        if bins['raw_materials'] > 0.0 and bins['misc'] > 0.0:
+            if bins['raw_materials'] < (0.5 * res[4]):
+                bins['misc'] += bins['raw_materials']
+                bins['raw_materials'] = 0.0
             else:
-                bin_goods += bin_services
-                bin_services = 0.0
+                bins['raw_materials'] += bins['misc']
+                bins['misc'] = 0.0
 
         # new belgian regulation about what is considered an investment to vat 
         # administration (see book)
-        if bin_investments < 1000.0:
-            bin_services += bin_investments
-            bin_investments = 0.0
-
-        # if we have an intracom credit note, correct the bins according to 
-        # belgian vat return logic
-        if is_intracom and res[3] < 0:
-            bin_cn_intracom = bin_cn
-            bin_cn = 0.0
-            bin_payable = 0.0
-
-        # if we have a credit note on an import (ex-EU), correct the bins
-        # according to belgian vat return logic
-        if is_import and res[3] < 0:
-            bin_payable = 0.0
+        if bins['investments'] < 1000.0:
+            bins['misc'] += bins['investments']
+            bins['investments'] = 0.0
 
         invoices.append([
             res[0],
             res[1],
             res[2],
             res[3],
-            bin_goods,
-            bin_services,
-            bin_investments,
-            bin_notax,
-            bin_cn,
-            res[3] if is_intracom else 0.0,
-            bin_cn_intracom,
-            bin_intracom,
-            bin_imports,
-            bin_refundable,
-            bin_payable
+            bins['raw_materials'],
+            bins['misc'],
+            bins['investments'],
+            "notax",
+            "cn",
+            "",
+            "",
+            bins['intracom_tax'],
+            bins['import_tax'],
+            bins['refundable'],
+            bins['non_refundable']
         ])
 
     return invoices
@@ -188,18 +176,16 @@ def get_bins(invoice_ref):
     query = "SELECT (SELECT grid_no FROM `tabAccount VAT Allocation` WHERE parent = a.name), e.debit, e.credit, a.name FROM `tabGL Entry` e INNER JOIN `tabAccount` a ON a.name = e.account WHERE voucher_no = '{}' AND a.account_number != '440'".format(invoice_ref)
     gl_entries = frappe.db.sql(query)
 
-    bin_goods = 0.0
-    bin_services = 0.0
-    bin_investments = 0.0
-    bin_cn = 0.0
-    bin_intracom = 0.0
-    bin_cn_intracom = 0.0
-    is_intracom = False
-    is_import = False
-    bin_imports = 0.0
-    bin_refundable = 0.0
-    bin_payable = 0.0
-    bin_notax = 0.0
+    bins = {
+        'raw_materials': 0.0,
+        'misc': 0.0,
+        'investments': 0.0,
+        'refundable': 0.0,
+        'non_refundable': 0.0,
+        'intracom_tax': 0.0,
+        'import_tax': 0.0,
+        'no_tax': 0.0
+    }
 
     for entry in gl_entries:
         grid_no = entry[0]
@@ -216,36 +202,25 @@ def get_bins(invoice_ref):
             res = frappe.db.sql(query)[0]
             curr_account = res[0]
             grid_no = res[1]
-
-        balance = entry[1] - entry[2]
         
         # belgian vat return logic
         if grid_no == "81":
-            bin_goods += balance
-            if balance < 0:
-                bin_cn += balance
+            bins['raw_materials'] += entry[1]
         elif grid_no == "82":
-            bin_services += balance
-            if balance < 0:
-                bin_cn += balance
+            bins['misc'] += entry[1]
         elif grid_no == "83":
-            bin_investments += balance
-            if balance < 0:
-                bin_cn += balance
+            bins['investments'] += entry[1]
         elif grid_no == "59":
-            if balance > 0:
-                bin_refundable += balance
-            else:
-                bin_payable += balance
+            bins['refundable'] += entry[1]
         elif grid_no == "55":
-            is_intracom = True
-            if balance < 0:
-                bin_intracom += balance
+            bins['intracom_tax'] += entry[1]
         elif grid_no == "57":
-            is_import = True
-            if balance < 0:
-                bin_imports += balance
+            bins['import_tax'] += entry[1]
+        elif grid_no == "998":
+            bins['no_tax'] += entry[1]
+        elif grid_no == "999":
+            bins['non_refundable'] += entry[1]
         else:
-            bin_notax += balance
+            frappe.throw("No VAT account for account {}".format(entry[3]))
 
-    return bin_goods, bin_services, bin_investments, bin_notax, abs(bin_cn), abs(bin_intracom), abs(bin_cn_intracom), is_intracom, is_import, abs(bin_imports), abs(bin_refundable), abs(bin_payable)
+    return bins
