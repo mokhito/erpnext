eb111ebcd39f35745cd1096e833619b938b3b841
mokhito <mokhito@users.noreply.github.com>
2019-04-24 12:01:41 +0200

    Implement Facturier des Ventes

--- erpnext/accounts/report/journal_des_ventes/journal_des_ventes.py
+++ erpnext/accounts/report/journal_des_ventes/journal_des_ventes.py
@@ -3,7 +3,8 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-
+from erpnext.controllers.taxes_and_totals import get_itemised_tax_breakup_data
+from decimal import Decimal
 
 def execute(filters=None):
     columns = [{
@@ -29,14 +30,14 @@ def execute(filters=None):
     },
     {
         "fieldname": "total_tvac",
-        "label": _("Total TVAC"),
+        "label": _("Total"),
         "fieldtype": "Float",
         "options": "",
         "width": 120
     },
     {
-        "fieldname": "grid_00",
-        "label": _("0%"),
+        "fieldname": "total_tvac",
+        "label": _("Net"),
         "fieldtype": "Float",
         "options": "",
         "width": 120
@@ -56,36 +57,22 @@ def execute(filters=None):
         "width": 120
     },
     {
-        "fieldname": "grid_49",
-        "label": _("Notes de crédit sauf intracom"),
+        "fieldname": "intracom_total",
+        "label": _("Ventes UE"),
         "fieldtype": "Float",
         "options": "",
         "width": 120
     },
     {
         "fieldname": "intracom_total",
-        "label": _("Ventes intracom"),
+        "label": _("TVA à payer"),
         "fieldtype": "Float",
         "options": "",
         "width": 120
     },
     {
-        "fieldname": "grid_48",
-        "label": _("Notes de crédit intracom"),
-        "fieldtype": "Float",
-        "options": "",
-        "width": 120
-    },
-    {
-        "fieldname": "vat_payable",
-        "label": _("TVA due"),
-        "fieldtype": "Float",
-        "options": "",
-        "width": 120
-    },
-    {
-        "fieldname": "vat_refundable",
-        "label": _("TVA à régulariser"),
+        "fieldname": "check",
+        "label": _("Total de contrôle"),
         "fieldtype": "Float",
         "options": "",
         "width": 120
@@ -102,7 +89,7 @@ def execute(filters=None):
 
 def get_invoices(filters):
     invoices = []
-    query = "SELECT posting_date, name, title, grand_total, net_total, territory, taxes_and_charges FROM `tabSales Invoice` WHERE status != 'Cancelled' AND status != 'Draft'"
+    query = "SELECT si.posting_date, si.name, si.title, si.grand_total, si.net_total, si.discount_amount, (SELECT country FROM `tabAddress` a WHERE name = si.customer_address) AS country FROM `tabSales Invoice` si WHERE si.status != 'Cancelled' AND si.status != 'Draft'"
 
     if filters.from_date is not None:
         query += " AND posting_date >= '{}'".format(filters.from_date)
@@ -112,87 +99,58 @@ def get_invoices(filters):
     res_invoices = frappe.db.sql(query)
 
     for res in res_invoices:
-        bin_0, bin_6, bin_21, bin_cn, bin_intracom, bin_cn_intracom, is_intracom, bin_refundable, bin_payable = get_bins(res[1])
+        bins = get_bins(res[1], res[6])
 
-        if res[5] == "Belgium" or res[5] == "All Territories":
-            if "6%" in res[6]:
-                bin_6 = bin_intracom
-            elif "21%" in res[6]:
-                bin_21 = res[4]
+        total_tvac = Decimal("{0:.2f}".format(res[3]))
+        total_control = bins['net'] + bins['6pc'] + bins['21pc']
         
-            bin_intracom = 0.0
-
         invoices.append([
             res[0],
             res[1],
             res[2],
-            res[3],
-            bin_0,
-            bin_6,
-            bin_21,
-            bin_cn,
-            bin_intracom,
-            bin_cn_intracom,
-            bin_payable,
-            bin_refundable
+            total_tvac,
+            bins['net'],
+            bins['6pc'],
+            bins['21pc'],
+            bins['intracom'],
+            bins['6pc'] + bins['21pc'],
+            total_control
         ])
 
     return invoices
 
-def get_bins(invoice_ref):
-    gl_entries = []
-    query = "SELECT (SELECT grid_no FROM `tabAccount VAT Allocation` WHERE parent = a.name), e.debit, e.credit, a.name FROM `tabGL Entry` e INNER JOIN `tabAccount` a ON a.name = e.account WHERE voucher_no = '{}' AND a.account_number != '440'".format(invoice_ref)
-    gl_entries = frappe.db.sql(query)
+def get_bins(invoice_ref, country):
+    bins = {
+        'net': 0.0,
+        '6pc': 0.0,
+        '21pc': 0.0,
+        'intracom': 0.0
+    }
 
-    bin_0 = 0.0
-    bin_6 = 0.0
-    bin_21 = 0.0
-    bin_cn = 0.0
-    bin_intracom = 0.0
-    bin_cn_intracom = 0.0
-    is_intracom = False
-    bin_refundable = 0.0
-    bin_payable = 0.0
+    doc = frappe.get_doc('Sales Invoice', invoice_ref)
+    taxes_breakup = get_itemised_tax_breakup_data(doc)
 
-    for entry in gl_entries:
-        grid_no = entry[0]
-        curr_account = entry[3]
+    if len(taxes_breakup[1]) > 0:
+        for item in taxes_breakup[1]:
+            bins['net'] += taxes_breakup[1][item]
 
-        # if account has no VAT grid number, check if its parents have one
-        while (grid_no is None):
-            
-            # we reached the root of the chart of accounts, can't go upper
-            if curr_account == "RK Group SPRL":
-                break
+    if country == "Belgium" or len(taxes_breakup[0]) > 0:
+        if len(taxes_breakup[0]) > 0:
+            for item in taxes_breakup[0]:
+                taxes = taxes_breakup[0][item]
+                for tax_name in taxes:
+                    if "T.V.A." in tax_name:
+                        if taxes[tax_name]['tax_rate'] == 6.0:
+                            bins['6pc'] += taxes[tax_name]['tax_amount']
+                        elif taxes[tax_name]['tax_rate'] == 21.0:
+                            bins['21pc'] += taxes[tax_name]['tax_amount']
+                    else:
+                        bins['net'] += taxes[tax_name]['tax_amount']
+    # TODO: check if in EU or not
+    else:
+        bins['intracom'] = bins['net']
 
-            query = "SELECT a.parent, (SELECT grid_no FROM `tabAccount VAT Allocation` WHERE parent = a.parent) AS grid_no FROM `tabAccount` a WHERE a.name = \"{}\"".format(curr_account)
-            res = frappe.db.sql(query)[0]
-            curr_account = res[0]
-            grid_no = res[1]
+    for bin in bins:
+        bins[bin] = Decimal("{0:.2f}".format(bins[bin]))
 
-        balance = entry[1] - entry[2]
-        
-        # belgian vat return logic
-        if grid_no == "01":
-            if balance > 0:
-                bin_cn += balance
-            else:
-                bin_6 += balance
-        elif grid_no == "03":
-            if balance > 0:
-                bin_cn += balance
-            else:
-                bin_21 += balance
-        elif grid_no == "46":
-            is_intracom = True
-            if balance > 0:
-                bin_cn_intracom += balance
-            else:
-                bin_intracom += balance
-        elif grid_no == "54":
-            if balance > 0:
-                bin_refundable += balance
-            else:
-                bin_payable += balance
-
-    return abs(bin_0), abs(bin_6), abs(bin_21), abs(bin_cn), abs(bin_intracom), bin_cn_intracom, is_intracom, bin_refundable, abs(bin_payable)
+    return bins
